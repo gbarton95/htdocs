4841ed9b0d2b31b2d5cbd12d7d303b40560eb6fa
Gabriela <119356728+gbarton95@users.noreply.github.com>
2024-03-07 15:53:50 +0100

    ch

--- laravel/examen_eva2/routes/web.php
+++ laravel/examen_eva2/routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('index');})->name('menu');
+
+Route::get('product', 'ProductController@index')->name('product.index');
+Route::get('product/id={product?}', 'ProductController@show')->name('product.show');
+Route::get('product/create', 'ProductController@create')->name('product.create');
+Route::post('product/{product?}', 'ProductController@store')->name('product.store');
+Route::get('product/{product}/edit', 'ProductController@edit')->name('product.edit');
+Route::patch('product/{product}', 'ProductController@update')->name('product.update');
+Route::delete('product/{product}', 'ProductController@destroy')->name('product.destroy');
+
+Route::get('supplier/products', 'SupplierController@products')->name('supplier.products');
+Route::get('order/form', 'OrderController@showForm')->name('order.form');
+Route::get('order/resume', 'OrderController@showResume')->name('order.resume');
+
+Route::resource('supplier', 'SupplierController');
+Route::resource('contact', 'ContactController');
+Route::resource('employee', 'EmployeeController');
+
+?>
