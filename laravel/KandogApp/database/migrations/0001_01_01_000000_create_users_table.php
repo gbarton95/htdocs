b86ab6066b0fd6bdd17162f8b8a2d9fd9c5972d8
Gabriela <bartoncio@gmail.com>
2024-04-24 09:40:43 +0200

    ch

--- laravel/KandogApp/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/KandogApp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,8 +11,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             //$table->integer('role'); para cuando haya más tipos de usuario
-            $table->string('nombre');
-            $table->string('apellidos');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
