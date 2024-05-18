e659ac7230b7035c99540d0ea11e5fa4cbeed4e0
Gabriela <bartoncio@gmail.com>
2024-05-18 17:08:14 +0200

    lenguaje okay

--- laravel/Kandog2/app/Http/Controllers/LocaleController.php
+++ laravel/Kandog2/app/Http/Controllers/LocaleController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class LocaleController extends Controller
 {
     public function setLocale($locale)
     {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user->locale = $locale;
+            $user->save();
+        }
+        
+        session(['locale' => $locale]);
         App::setLocale($locale);
-        Session::put("locale", $locale);
-
+        
         return redirect()->back();
     }
 }
--- laravel/Kandog2/app/Http/Middleware/LocaleMiddleware.php
+++ laravel/Kandog2/app/Http/Middleware/LocaleMiddleware.php
@@ -6,18 +6,19 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class LocaleMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
 
-        if(Session::get("locale")!=null){
-            App::setLocale(Session::get("locale"));
-        }else{
-            Session::put("locale", "en");
-            App::setLocale(Session::get("locale"));
+        if (Auth::check()) {
+            $locale = Auth::user()->locale;
+            App::setLocale($locale);
+        } else {
+            $locale = session('locale', config('app.locale'));
+            App::setLocale($locale);
         }
 
 
